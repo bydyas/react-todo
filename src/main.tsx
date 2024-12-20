4ab180615102d11deae04efbace713557cf913df
Bohdan Shkaran <bohdan.shkaran@todor3d.tech>
2024-12-20 11:39:53 +0200

    feat: twa setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { HashRouter } from 'react-router-dom';
+import { TwaAnalyticsProvider } from '@tonsolutions/telemetree-react';
+
 import './assets/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <TwaAnalyticsProvider
+        projectId={import.meta.env.VITE_TWA_PROJECT_ID}
+        apiKey={import.meta.env.VITE_TWA_API_KEY}
+        appName={import.meta.env.VITE_TWA_APP_NAME}
+      >
+        <App />
+      </TwaAnalyticsProvider>
     </HashRouter>
   </React.StrictMode>,
 );
