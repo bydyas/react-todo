e3f334000e839c78ff2e3bcd1c81566f684e4650
Bohdan Shkaran <bohdan.shkaran@todor3d.tech>
2024-12-20 12:09:35 +0200

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+// import App from './App.tsx';
 import { HashRouter } from 'react-router-dom';
 import { TwaAnalyticsProvider } from '@tonsolutions/telemetree-react';
 
